cc7577e5c6e37fbb9f142b2b58654e3078619f6f
himanshu4327 <110589449+himanshu4327@users.noreply.github.com>
2023-10-17 14:51:15 +0530

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -1,1 +1,486 @@
+SELECT * (or specifiy the column you want to select,"c1","c2" )
+FROM (specify the table from which you want to select data,"t1")
+JOIN  (specify the table  which you want to join ,"t2")
+ON (t1.c1 = t2.c2)
+WHERE (constraint expression)
+GROUP BY ("column")
+HAVING ("constraint_expression")
+ORDER BY ("c asc/desc")
+LIMIT count;
+
+
+1. The SELECT statement is used to specify the columns you want to retrieve from one or more tables.
+Example:
+Suppose you have two tables, employees and departments. You want to select the employee_name and department_name of employees who work in the 'Sales' department
+
+SELECT employee_name, department_name
+
+2.FROM:
+
+The FROM clause specifies the table from which you want to select data. You can specify multiple tables if you need to join them.
+Example:
+Continuing from the previous example, you'd specify the employees and departments tables in the FROM clause.
+  
+FROM employees
+
+3.The JOIN clause allows you to combine rows from two or more tables based on a related column between them.
+Example:
+You want to join the employees table with the departments table using the department_id column.
+  JOIN departments
+ON employees.department_id = departments.department_id
+
+4. WHERE:
+The WHERE clause is used to filter rows based on specified conditions.
+Example:
+You want to retrieve employees from the 'Sales' department only.
+  WHERE department_name = 'Sales'
+
+5. The GROUP BY clause is used to group rows with the same values in one or more columns.
+Example:
+If you want to group employees by their department_name, you'd use:
+
+GROUP BY department_name
+
+6. The HAVING clause filters the results of the grouping.
+Example:
+If you want to retrieve departments with more than five employees, you'd use:
+  HAVING COUNT(*) > 5
+
+7.  ORDER BY:
+The ORDER BY clause sorts the result set by one or more columns, either in ascending (ASC) or descending (DESC) order.
+Example:
+If you want to order the departments alphabetically
+ORDER BY department_name ASC
+
+8. LIMIT:
+
+The LIMIT clause is used to limit the number of rows returned in the result set.
+Example:
+If you want to see the first five departments:
+LIMIT 5
+
+SELECT employee_name, department_name
+FROM employees
+JOIN departments
+ON employees.department_id = departments.department_id
+WHERE department_name = 'Sales'
+GROUP BY department_name
+HAVING COUNT(*) > 5
+ORDER BY department_name ASC
+LIMIT 5;
+
+
+example:
+SELECT employees.employee_name, departments.department_name
+FROM employees
+JOIN departments
+ON employees.department_id = departments.department_id
+WHERE departments.department_name = 'Sales'
+GROUP BY departments.department_name
+HAVING COUNT(employees.employee_name) > 5
+ORDER BY departments.department_name ASC
+LIMIT 5;
+
+JOINS:
+
+1. INNER JOIN:
+An INNER JOIN retrieves rows that have matching values in both tables.
+Example:
+Suppose you have two tables, employees and departments. You want to retrieve a list of employees and their respective departments.
+ 
+  SELECT employees.employee_name, departments.department_name
+FROM employees
+INNER JOIN departments
+ON employees.department_id = departments.department_id;
+
+2. LEFT JOIN (or LEFT OUTER JOIN):
+A LEFT JOIN retrieves all rows from the left table and the matched rows from the right table. If there is no match, NULL values are returned for columns from the right table.
+Example:
+You want to retrieve a list of all employees and their respective departments, including employees without assigned departments.
+
+  SELECT employees.employee_name, departments.department_name
+FROM employees
+LEFT JOIN departments
+ON employees.department_id = departments.department_id;
+
+3.  RIGHT JOIN (or RIGHT OUTER JOIN):
+
+A RIGHT JOIN is similar to a LEFT JOIN, but it retrieves all rows from the right table and the matched rows from the left table.
+Example:
+You want to retrieve a list of all departments and the employees assigned to them, including departments with no employees.
+
+  SELECT employees.employee_name, departments.department_name
+FROM employees
+RIGHT JOIN departments
+ON employees.department_id = departments.department_id;
+
+4. FULL JOIN (or FULL OUTER JOIN):
+
+A FULL JOIN retrieves all rows from both tables, with NULL values in columns where there is no match.
+Example:
+You want to retrieve a list of all employees and departments, including employees without assigned departments and departments with no employees.
+
+  SELECT employees.employee_name, departments.department_name
+FROM employees
+FULL JOIN departments
+ON employees.department_id = departments.department_id;
+
+5.  SELF JOIN:
+
+A SELF JOIN is used to join a table with itself. It's often used to retrieve hierarchical data, such as employees and their managers.
+Example:
+Suppose you have an employees table with a manager_id column representing the manager of each employee. You want to retrieve a list of employees and their managers.
+
+  SELECT e.employee_name, m.employee_name AS manager_name
+FROM employees e
+LEFT JOIN employees m
+ON e.manager_id = m.employee_id;
+
+6. CROSS JOIN:
+
+A cross join, also known as a Cartesian product, is a type of join in SQL that combines all rows from one table with all rows from another table. This results in every possible combination of rows between the two tables. Cross joins are used less frequently than other join types, as they can produce a very large result set, especially when working with tables of significant size.
+
+Here's an explanation of a cross join with an example:
+
+Syntax:
+
+sql:
+SELECT * 
+FROM table1
+CROSS JOIN table2;
+
+//In MySQL, as in many other database management systems, data types are used to define what type of data can be stored in a table's columns. Data types provide a way to ensure data integrity and consistency within the database. MySQL offers a wide range of data types to accommodate various types of data, from integers and floating-point numbers to strings, dates, and more. Here are some common data types in MySQL:
+
+**Numeric Data Types:**
+
+1. **INT or INTEGER:** This data type is used to store whole numbers. It can be signed (positive and negative values) or unsigned (only positive values).
+
+   Example: `INT`, `INT(11)`, `INT UNSIGNED`.
+
+2. **TINYINT, SMALLINT, MEDIUMINT, BIGINT:** These are also used for storing integers, but they have different size and storage requirements. TINYINT is for very small numbers, while BIGINT is for very large ones.
+
+   Example: `TINYINT`, `SMALLINT`, `MEDIUMINT`, `BIGINT`.
+
+3. **DECIMAL or NUMERIC:** These are used for exact numeric values, such as monetary amounts, where precision and scale can be defined.
+
+   Example: `DECIMAL(10, 2)`, `NUMERIC(6, 3)`.
+
+4. **FLOAT and DOUBLE:** These data types are used for floating-point numbers with single-precision (FLOAT) or double-precision (DOUBLE) accuracy.
+
+   Example: `FLOAT`, `DOUBLE`.
+
+**Text Data Types:**
+
+5. **CHAR:** This is used for fixed-length strings. It stores a specific number of characters.
+
+   Example: `CHAR(10)`.
+
+6. **VARCHAR:** This is used for variable-length strings. It stores a variable number of characters up to a maximum length.
+
+   Example: `VARCHAR(255)`.
+
+7. **TEXT and LONGTEXT:** These data types are used for storing large amounts of text data, with LONGTEXT accommodating even larger text.
+
+   Example: `TEXT`, `LONGTEXT`.
+
+**Date and Time Data Types:**
+
+8. **DATE:** This data type is used for storing dates in 'YYYY-MM-DD' format.
+
+   Example: `DATE`.
+
+9. **TIME:** It is used for storing time values in 'HH:MM:SS' format.
+
+   Example: `TIME`.
+
+10. **DATETIME and TIMESTAMP:** These data types store both date and time values. TIMESTAMP is used for recording time with automatic updates when a row is inserted or updated.
+
+   Example: `DATETIME`, `TIMESTAMP`.
+
+**Binary Data Types:**
+
+11. **BLOB and LONGBLOB:** These data types are used to store binary large objects, such as images or binary files.
+
+   Example: `BLOB`, `LONGBLOB`.
+
+**Miscellaneous Data Types:**
+
+12. **BOOLEAN or BOOL:** This data type is used for representing true or false values.
+
+   Example: `BOOLEAN`, `BOOL`.
+
+13. **ENUM:** ENUM allows you to store one of a predefined set of values.
+
+   Example: `ENUM('Low', 'Medium', 'High')`.
+
+14. **SET:** SET allows you to store zero or more values from a predefined set of values.
+
+   Example: `SET('Red', 'Green', 'Blue')`.
+
+These are some of the commonly used data types in MySQL. The choice of data type depends on the nature of the data you want to store and the specific requirements of your database schema. Selecting the appropriate data type ensures data integrity, efficient storage, and optimized query performance.
+
+//aggregation :=>
+
+*  In MySQL, aggregation refers to the process of performing calculations on a set of rows to obtain a single result or summary of the data. Aggregation is typically used to derive meaningful insights from large datasets by summarizing, grouping, and performing calculations on data in one or more columns. Common aggregation functions in MySQL include SUM, COUNT, AVG, MIN, and MAX.
+
+Here's an explanation of some common aggregation functions and how they work in MySQL:
+
+* SUM: The SUM function is used to calculate the total of all values in a numeric column. It adds up the values in the specified column.
+
+Example:
+sql:
+SELECT SUM(sales_amount) AS total_sales
+FROM sales_data;
+
+* COUNT: The COUNT function is used to count the number of rows that match a specific condition. It can be used to count all rows or rows meeting certain criteria.
+Example:
+sql:
+SELECT COUNT(*) AS total_orders
+FROM orders;
+
+* AVG: The AVG function calculates the average (mean) of the values in a numeric column.
+Example:
+sql:
+SELECT AVG(product_rating) AS average_rating
+FROM products;
+
+* MIN: The MIN function returns the minimum value in a numeric or date column.
+Example:
+sql:
+SELECT MIN(salary) AS lowest_salary
+FROM employees;
+
+* MAX: The MAX function returns the maximum value in a numeric or date column.
+Example:
+sql:
+SELECT MAX(stock_price) AS highest_price
+FROM stocks;
+
+* Example with GROUP BY:
+sql:
+SELECT product_category, SUM(sales_amount) AS total_sales
+FROM sales_data
+GROUP BY product_category;
+
+// crud :
+1. INSERT INTO customers (customer_name, email, phone)
+VALUES ('John Doe', 'johndoe@email.com', '123-456-7890');
+2. SELECT * FROM products WHERE price < 50;
+3. UPDATE employees
+SET salary = 60000
+WHERE department = 'Sales';
+4. DELETE FROM orders
+WHERE order_date < '2023-01-01';
+
+
+//IN operator:
+In MySQL, the IN operator is used to specify a list of values as a condition in a WHERE clause, allowing you to filter rows based on whether a column's value matches any of the values in the provided list. The IN operator simplifies complex WHERE conditions when you want to compare a single column to multiple values.
+
+SELECT column1, column2, ...
+FROM table_name
+WHERE column_name IN (value1, value2, ...);
+
+//
+SELECT product_name, category
+FROM products
+WHERE category IN ('Electronics', 'Clothing', 'Books');
+
+
+// operator:
+In MySQL and most SQL database systems, operators are used to perform various operations in SQL statements, such as filtering data, performing calculations, and comparing values. Here's an overview of some common operators in MySQL:
+  
+1. Arithmetic Operators:
+
+Arithmetic operators are used to perform mathematical calculations.
+
++ (Addition): Adds two values.
+
+- (Subtraction): Subtracts one value from another.
+
+* (Multiplication): Multiplies two values.
+
+/ (Division): Divides one value by another.
+
+% (Modulus): Returns the remainder of a division operation.
+
+Example:
+sql:
+SELECT salary + bonus AS total_income
+FROM employees;
+
+Comparison Operators:
+
+Comparison operators are used to compare values and return a Boolean result (True or False).
+
+= (Equal to): Compares if two values are equal.
+
+!= or <> (Not equal to): Compares if two values are not equal.
+
+< (Less than): Checks if one value is less than another.
+
+> (Greater than): Checks if one value is greater than another.
+
+<= (Less than or equal to): Checks if one value is less than or equal to another.
+
+>= (Greater than or equal to): Checks if one value is greater than or equal to another.
+
+Example:
+sql:
+SELECT product_name
+FROM products
+WHERE price < 50;
+
+3. Logical Operators:
+
+Logical operators are used to combine conditions and create more complex conditions.
+
+AND (Logical AND): Returns true if all specified conditions are true.
+
+OR (Logical OR): Returns true if at least one of the specified conditions is true.
+
+NOT (Logical NOT): Negates a condition.
+
+Example:
+sql:
+SELECT order_id, order_date
+FROM orders
+WHERE order_total > 1000 AND (order_status = 'Shipped' OR order_status = 'Delivered');
+
+
+4. Concatenation Operator:
+
+The || operator (or CONCAT() function) is used to concatenate (combine) two or more strings.
+Example:
+sql:
+SELECT first_name || ' ' || last_name AS full_name
+FROM employees;
+
+5. Assignment Operator:
+
+The = operator is used for assignment, such as when setting a variable's value or updating a column's value.
+
+6. Like Operator:
+
+The LIKE operator is used for pattern matching in text columns. It allows you to search for a specified pattern within a column.
+Example:
+sql:
+SELECT product_name
+FROM products
+WHERE product_name LIKE 'Ap%';  -- Matches product names starting with 'Ap'
+
+//indexing 
+Indexing is a database optimization technique used to improve the speed and efficiency of data retrieval from database tables. Indexes are data structures that store a subset of the table's data, making it quicker to look up and access specific rows. Indexes are particularly useful when working with large tables, as they significantly reduce the time required to search for data. Here's an explanation of indexing with an example:
+
+Indexing in MySQL:
+
+In MySQL, you can create indexes on one or more columns of a table using the CREATE INDEX or ALTER TABLE statement.
+
+Common types of indexes include primary keys, unique keys, and regular (non-unique) indexes.
+
+Example:
+
+Suppose you have a table called employees with the following structure:
+
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    hire_date DATE
+);
+
+Now, let's say you frequently need to retrieve employee information based on their employee_id. Creating an index on the employee_id column can significantly improve query performance:
+
+CREATE INDEX idx_employee_id ON employees (employee_id);
+
+With this index in place, querying for employee information based on employee_id becomes much faster. For example:
+
+SELECT first_name, last_name
+FROM employees
+WHERE employee_id = 101;
+
+The database engine can use the index to quickly locate the row with employee_id 101, resulting in a faster query execution.
+
+
+##Types of Indexes:
+
+* Primary Key Index:
+
+A primary key index enforces the uniqueness of values in a column and is used to identify each row uniquely. By default, a primary key creates a unique index.
+Example:
+sql
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50)
+);
+
+* Unique Index:
+A unique index enforces uniqueness for values in a column, but it allows one NULL value. You can create a unique index explicitly.
+Example:
+sql
+CREATE TABLE customers (
+    customer_id INT,
+    email VARCHAR(100) UNIQUE
+);
+
+* Regular (Non-Unique) Index:
+A regular index improves query performance but doesn't enforce uniqueness.
+Example:
+sql
+CREATE INDEX idx_employee_name ON employees (last_name, first_name);
+Full-Text Index:
+
+* A full-text index is used for efficient text-based searching. It's commonly used for searching large text fields.
+Example:
+sql
+CREATE FULLTEXT INDEX idx_product_description ON products (product_description);
+
+ *Composite Index:
+A composite index involves multiple columns and is used for improving query performance when searching using multiple criteria.
+Example:
+sql
+CREATE INDEX idx_employee_name ON employees (last_name, first_name);
+Indexes can significantly improve query performance, but they do come with trade-offs. They consume disk space, and they slow down write operations (e.g., inserts, updates, and deletes) because the index needs to be updated. Therefore, it's essential to create indexes judiciously and consider the specific requirements of your database and queries.
+
+## transaction
+In the context of a database management system like MySQL, a transaction is a sequence of one or more SQL statements that are executed as a single unit of work. Transactions ensure that a series of related database operations either all succeed or all fail, providing data consistency and integrity. If any part of a transaction fails, the entire transaction is rolled back, and the database is left in a state as if the transaction never occurred. Transactions are often used in situations where multiple operations must be atomic, ensuring that data remains consistent.
+Here's an explanation of transactions with an example:
+
+Example of a Transaction:
+
+Suppose you have a database that stores information about bank accounts and transactions. You want to transfer money from one account to another while maintaining data integrity.
+
+sql
+-- Start a transaction
+START TRANSACTION;
+
+-- Step 1: Subtract money from one account
+UPDATE accounts
+SET balance = balance - 500
+WHERE account_number = '12345';
+
+-- Step 2: Add money to another account
+UPDATE accounts
+SET balance = balance + 500
+WHERE account_number = '67890';
+
+-- Commit the transaction if both steps are successful
+COMMIT;
+=>>
+In this example:
+
+We begin the transaction with START TRANSACTION;. This marks the beginning of the transaction, and any changes made to the database are temporary until the transaction is committed.
+
+We update the balance of one account by subtracting $500 (Step 1).
+
+We update the balance of another account by adding $500 (Step 2).
+
+Finally, if both steps are successful and no errors occur, we commit the transaction using COMMIT;. This means the changes are permanently saved to the database, and the transaction is considered complete.
+
+If any issue arises during the transaction (e.g., an error in one of the updates), you can use ROLLBACK; to undo all changes made during the transaction and return the database to its previous state. For example:
+
+sql
+-- If an error occurs during the transaction
+-- Roll back the changes
+ROLLBACK;
+By using transactions, you ensure that the transfer of money between accounts is an atomic operation. Either both updates succeed and the transaction is committed, or if an error occurs at any point, the entire transaction is rolled back, and the database remains unchanged. This helps maintain data integrity and consistency in critical scenarios like financial transactions.
 
